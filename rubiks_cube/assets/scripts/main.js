3ee5c0c0b0d5b7afd4adc07a950d0a4cb7e014b5
LuccamvmRey <luccamvmachadorey@gmail.com>
2022-07-12 20:58:46 -0300

    conclusão de rotação

--- rubiks_cube/assets/scripts/main.js
+++ rubiks_cube/assets/scripts/main.js
@@ -9,6 +9,13 @@ const sePieces = document.querySelectorAll(".s");
 const equaPieces = document.querySelectorAll(".e");
 const pieces = document.querySelectorAll(".piece");
 
+const fOne = document.querySelectorAll(".f-one .piece");
+const fTwo = document.querySelectorAll(".f-two .piece");
+const fThree = document.querySelectorAll(".f-three .piece");
+const fFour = document.querySelectorAll(".f-four .piece");
+const fFive = document.querySelectorAll(".f-five .piece");
+const fSix = document.querySelectorAll(".f-six .piece");
+
 const upPieceMap = [];
 const downPieceMap = [];
 const rightPieceMap = [];
@@ -18,6 +25,13 @@ const backPieceMap = [];
 const miPieceMap = [];
 const sePieceMap = [];
 const equaPieceMap = [];
+
+const fOneMap = [];
+const fTwoMap = [];
+const fThreeMap = [];
+const fFourMap = [];
+const fFiveMap = [];
+const fSixMap = [];
 
 const maps = [
   upPieceMap,
@@ -31,6 +45,8 @@ const maps = [
   equaPieceMap,
 ];
 
+const faceMaps = [fOneMap, fTwoMap, fThreeMap, fFourMap, fFiveMap, fSixMap];
+
 const movePieces = [
   upPieces,
   downPieces,
@@ -42,6 +58,8 @@ const movePieces = [
   sePieces,
   equaPieces,
 ];
+
+const facePieces = [fOne, fTwo, fThree, fFour, fFive, fSix];
 
 const setColor = () => {
   pieces.forEach((item) => {
@@ -94,32 +112,54 @@ const setColor = () => {
       });
     });
   };
+
+  const mapAllFaces = () => {
+    facePieces.forEach((face) => {
+      face.forEach((item) => {
+        const pieceColor = parseInt(item.getAttribute("data-color"));
+        if (face === fOne) {
+          fOneMap.push(pieceColor);
+        } else if (face === fTwo) {
+          fTwoMap.push(pieceColor);
+        } else if (face === fThree) {
+          fThreeMap.push(pieceColor);
+        } else if (face === fFour) {
+          fFourMap.push(pieceColor);
+        } else if (face === fFive) {
+          fFiveMap.push(pieceColor);
+        } else if (face === fSix) {
+          fSixMap.push(pieceColor);
+        }
+      });
+    });
+  };
+  mapAllFaces();
   mapAll();
 };
 
-const move = (moveMap, movePieces, type ) => {
+const move = (moveMap, movePieces, type) => {
   let newColors = [];
 
-  if (type === "reg") {
+  if (type === "clock") {
     moveMap.forEach((p, idx) => {
       if (idx >= 9) {
         newColors.push(moveMap[idx - 9]);
       } else {
         newColors.push(moveMap[idx + 3]);
       }
-    })
-  } else if (type === "prime") {
+    });
+  } else if (type === "counter") {
     moveMap.forEach((p, idx) => {
       if (idx <= 2) {
         newColors.push(moveMap[idx + 9]);
       } else {
         newColors.push(moveMap[idx - 3]);
       }
-    })
+    });
   }
 
   newColors.forEach((color, idx) => {
-    for (let i = 0; i <= newColors.length;) {
+    for (let i = 0; i <= newColors.length; ) {
       if (i === idx) {
         movePieces[i].setAttribute("data-color", `${color}`);
         break;
@@ -127,25 +167,197 @@ const move = (moveMap, movePieces, type ) => {
         i++;
       }
     }
-  })
+  });
+
+  const doRotate = () => {
+    let rot;
+    
+    const cisRot = () => {
+      if (type === "clock") {
+        rot = "clock";
+      } else {
+        rot = "counter"
+      }
+    }
+
+    const transRot = () => {
+      if (type === "clock") {
+        rot = "counter";
+      } else {
+        rot = "clock"
+      }
+    }
+
+    if (movePieces === upPieces) {
+      cisRot()
+      rotate(fOneMap, rot);
+    } else if (movePieces === downPieces) {
+      transRot()
+      rotate(fFiveMap, rot);
+    } else if (movePieces === rightPieces) {
+      cisRot()
+      rotate(fFourMap, rot);
+    } else if (movePieces === leftPieces) {
+      transRot();
+      rotate(fTwoMap, rot);
+    } else if (movePieces === frontPieces) {
+      cisRot();
+      rotate(fThreeMap, rot);
+    } else if (movePieces === backPieces) {
+      transRot();
+      rotate(fSixMap, rot);
+    }
+    rot = "";
+  };
 
   setColor();
-  maps.forEach(map => {
-    map.splice(0, 12);
-  })
+  trimMaps();
+  doRotate();
   newColors = [];
-}
+};
+
+const rotate = (faceMap, type) => {
+  let newFaceColors = [];
+  let face;
+
+  if (faceMap === fOneMap) {
+    face = fOne;
+  } else if (faceMap === fTwoMap) {
+    face = fTwo;
+  } else if (faceMap === fThreeMap) {
+    face = fThree;
+  } else if (faceMap === fFourMap) {
+    face = fFour;
+  } else if (faceMap === fFiveMap) {
+    face = fFive;
+  } else if (faceMap === fSixMap) {
+    face = fSix;
+  }
+
+  if (type === "counter") {
+    faceMap.forEach((p, idx) => {
+      if (idx == 0) {
+        newFaceColors.push(faceMap[2]);
+      } else if (idx == 1) {
+        newFaceColors.push(faceMap[5]);
+      } else if (idx == 2) {
+        newFaceColors.push(faceMap[8]);
+      } else if (idx == 3) {
+        newFaceColors.push(faceMap[1]);
+      } else if (idx == 4) {
+        newFaceColors.push(faceMap[4]);
+      } else if (idx == 5) {
+        newFaceColors.push(faceMap[7]);
+      } else if (idx == 6) {
+        newFaceColors.push(faceMap[0]);
+      } else if (idx == 7) {
+        newFaceColors.push(faceMap[3]);
+      } else if (idx == 8) {
+        newFaceColors.push(faceMap[6]);
+      }
+    });
+  } else if (type === "clock") {
+    faceMap.forEach((p, idx) => {
+      if (idx == 0) {
+        newFaceColors.push(faceMap[6]);
+      } else if (idx == 1) {
+        newFaceColors.push(faceMap[3]);
+      } else if (idx == 2) {
+        newFaceColors.push(faceMap[0]);
+      } else if (idx == 3) {
+        newFaceColors.push(faceMap[7]);
+      } else if (idx == 4) {
+        newFaceColors.push(faceMap[4]);
+      } else if (idx == 5) {
+        newFaceColors.push(faceMap[1]);
+      } else if (idx == 6) {
+        newFaceColors.push(faceMap[8]);
+      } else if (idx == 7) {
+        newFaceColors.push(faceMap[5]);
+      } else if (idx == 8) {
+        newFaceColors.push(faceMap[2]);
+      }
+    });
+  }
+
+  newFaceColors.forEach((color, idx) => {
+    for (let i = 0; i <= newFaceColors.length; ) {
+      if (i === idx) {
+        face[i].setAttribute("data-color", `${color}`);
+        break;
+      } else {
+        i++;
+      }
+    }
+  });
+
+  setColor();
+  trimMaps();
+  newFaceColors = [];
+  face = "";
+};
+
+const trimMaps = () => {
+  faceMaps.forEach((map) => {
+    map.splice(0, 9);
+  });
+  maps.forEach((map) => {
+    map.splice(0, 12);
+  });
+};
 
 setColor();
 document.getElementById("up").addEventListener("click", () => {
-  move(upPieceMap, upPieces, "reg");
+  move(upPieceMap, upPieces, "clock");
 });
 document.getElementById("up-prime").addEventListener("click", () => {
-  move(upPieceMap, upPieces, "prime");
+  move(upPieceMap, upPieces, "counter");
+});
+document.getElementById("down").addEventListener("click", () => {
+  move(downPieceMap, downPieces, "counter");
+});
+document.getElementById("down-prime").addEventListener("click", () => {
+  move(downPieceMap, downPieces, "clock");
 });
 document.getElementById("right").addEventListener("click", () => {
-  move(rightPieceMap, rightPieces, "reg");
+  move(rightPieceMap, rightPieces, "clock");
 });
 document.getElementById("right-prime").addEventListener("click", () => {
-  move(rightPieceMap, rightPieces, "prime");
+  move(rightPieceMap, rightPieces, "counter");
 });
+document.getElementById("left").addEventListener("click", () => {
+  move(leftPieceMap, leftPieces, "counter");
+});
+document.getElementById("left-prime").addEventListener("click", () => {
+  move(leftPieceMap, leftPieces, "clock");
+});
+document.getElementById("front").addEventListener("click", () => {
+  move(frontPieceMap, frontPieces, "clock");
+});
+document.getElementById("front-prime").addEventListener("click", () => {
+  move(frontPieceMap, frontPieces, "counter");
+});
+document.getElementById("back").addEventListener("click", () => {
+  move(backPieceMap, backPieces, "counter");
+});
+document.getElementById("back-prime").addEventListener("click", () => {
+  move(backPieceMap, backPieces, "clock");
+});
+document.getElementById("middle").addEventListener("click", () => {
+  move(miPieceMap, miPieces, "counter");
+});
+document.getElementById("middle-prime").addEventListener("click", () => {
+  move(miPieceMap, miPieces, "clock");
+});
+// document.getElementById("setentrional").addEventListener("click", () => {
+//   move(sePieceMap, sePieces, "clock");
+// });
+// document.getElementById("setentrional-prime").addEventListener("click", () => {
+//   move(sePieceMap, sePieces, "counter");
+// });
+// document.getElementById("equatorial").addEventListener("click", () => {
+//   move(equaPieceMap, equaPieces, "counter");
+// });
+// document.getElementById("equatorial-prime").addEventListener("click", () => {
+//   move(equaPieceMap, equaPieces, "clock");
+// });
---
+++ rubiks_cube/assets/scripts/spare.js
@@ -0,0 +1,13 @@
+if (move === fOne) {
+  fOneMap.push(pieceColor);
+} else if (move === fTwo) {
+  fTwoMap.push(pieceColor);
+} else if (move === fThree) {
+  fThreeMap.push(pieceColor);
+} else if (move === fFour) {
+  fFourMap.push(pieceColor);
+} else if (move === fFive) {
+  fFiveMap.push(pieceColor);
+} else if (move === fSix) {
+  fSixMap.push(pieceColor);
+}
